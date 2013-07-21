76ac85f2fa9337ccb2214702b889346fcf285e0c
Alex Yanovsky <ayanovsk@gmail.com>
2013-07-21 02:29:32 +0000

    Edited product desc in index.html

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ var str = buf.toString();
   response.send(str);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
